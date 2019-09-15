e6046eed1676d04bb46fd2a138181701fdb3bbf5
meliduong13 <meliduong13@gmail.com>
2019-09-14 23:54:07 -0400

    Corrected error with the words list having spaces before/after a word, therefore not matching with article content

--- LAB2.py
+++ LAB2.py
@@ -7,15 +7,16 @@ filename = "all-places-strings.lc.txt"
 
 # currently works with a single file
 # read article
-with open("reut2-000.sgm") as fp:
+with open("reut2-0000.sgm") as fp:
     soup = BeautifulSoup(fp, "html.parser")
 
 # get all words to look for from word doc
 with open(filename, "r") as fd:
     words_list = fd.read().splitlines()
     for word in words_list:
+        word = word.rstrip()
         inverted_index[word] = []
-print(words_list)
+    print(inverted_index)
 
 
 def clean_up_text(string_input):
@@ -36,9 +37,12 @@ for each in articles:
             newid_dict[each['newid']] = ""
 
 for newid in newid_dict:
-    for word in words_list:
+    print('newid is: ' + newid)
+    for word in inverted_index:
         if any(word in each_article_word for each_article_word in newid_dict[newid]):
             inverted_index[word].append(newid)
+        # if any('romania' in each_article_word for each_article_word in newid_dict[newid]):
+        #     print('found romania')
 
 # prints the dictionary that associates the words in the all-places-strings.lc.txt to the newid which contain the given words
 for key in inverted_index:
@@ -46,3 +50,6 @@ for key in inverted_index:
     for each in inverted_index[key]:
         print(each, end=", ")
     print('\n')
+
+# print('****')
+# print(newid_dict['925'])
